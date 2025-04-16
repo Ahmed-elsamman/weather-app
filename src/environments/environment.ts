5bd10392ae452c39b9fcb541c0ce4470cb2cf5d5
ahmed-elsamman-ayen <ahmed.elsamman@ayen.app>
2025-04-16 21:31:12 +0200

    add current, daily, and hourly forecast components with environment configuration

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    api: 'https://www.meteosource.com/api/v1/free/point?place_id=london&sections=all&timezone=UTC&language=en&units=metric&key=vwaj19gzgy2jdvb3zldxsujqftctmx9p6kril7hy',
+
+};
